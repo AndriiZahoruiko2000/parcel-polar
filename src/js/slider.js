ac6180d3abb2459ac76557f16bf8bf78415eeebc
Andrii2715 <zahoruiko.andrii17@gmail.com>
2022-09-25 12:37:19 +0300

    parcel-done

---
+++ src/js/slider.js
@@ -0,0 +1,9 @@
+$('.delivery__list').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 3,
+  nextArrow: '.delivery__btn-right',
+  prevArrow: '.delivery__btn-left',
+  autoplay: true,
+  autoplaySpeed: 1500,
+});
